55b76a567647b73d53b0353b7a2570420b092548
Vishal Mugutrao <G558363@GENMILLS.COM>
2020-01-10 18:48:54 +0530

    Updated

--- KaprekarsConstant.ps1
+++ KaprekarsConstant.ps1
@@ -2,7 +2,7 @@ Function KaprekarsConstant($num)
     {
     $i = 0
     [String]$num1 = $num
-    ($num1[0..($num1.length)]) | % { if((($num1.Split("$_")).count-1) -gt 2){Write-Host "More that 2 duplicate digit";break}}
+    ($num1[0..($num1.length)]) | ForEach-Object { if((($num1.Split("$_")).count-1) -gt 2){Write-Host "More that 2 duplicate digit";break}}
     If($num1.length -le 2 -or $num1.Length -gt 4 )
         {
         Write-Host "number should be minimum 3 and maximum 4 digit, [$Num]" -ForegroundColor Red
