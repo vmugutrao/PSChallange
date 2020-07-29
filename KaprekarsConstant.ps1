8e6c115f438245d12f41d3de6449d758e9efad0a
Vishal <33682067+vmugutrao@users.noreply.github.com>
2020-07-29 13:26:44 +0530

    update KaprekarsConstant.ps1

--- KaprekarsConstant.ps1
+++ KaprekarsConstant.ps1
@@ -2,7 +2,6 @@ Function KaprekarsConstant($num)
     {
     $i = 0
     [String]$num1 = $num
-    ($num1[0..($num1.length)]) | ForEach-Object { if((($num1.Split("$_")).count-1) -gt 2){Write-Host "More that 2 duplicate digit";break}}
     If($num1.length -le 2 -or $num1.Length -gt 4 )
         {
         Write-Host "number should be minimum 3 and maximum 4 digit, [$Num]" -ForegroundColor Red
@@ -21,7 +20,7 @@ Function KaprekarsConstant($num)
         [int]$AseNum = ($num1[0..($num1.length)] | Sort) -join ''
         [int]$num1 = $DesNum - $AseNum
         Write-Host "($i) $($DesNum) - $($AseNum) = $($num1)"
-    }Until($num1 -eq "6174")
+    }Until($num1 -eq "6174" -or $num1 -eq 0)
     if($i -gt 2)
         {
         Write-Host "Input : $num" -ForegroundColor Green
@@ -31,5 +30,4 @@ Function KaprekarsConstant($num)
         {
         Write-Host 'Invalid input' -ForegroundColor Red
     }
-    
 }
